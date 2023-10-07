53a24d5da2e2e23a0ac8cde58ead8961efdd6e57
Michael S <37552609+tonichaelmight@users.noreply.github.com>
2023-10-06 22:18:39 -0400

    Update ev.js

--- ev.js
+++ ev.js
@@ -1,22 +1,2 @@
-// channel and bot variables
-export const BOT_NAME = 'bexxtebot';
-
-// API TOKENS: DO NOT CHANGE!!!!
-export const TWITCH_OAUTH_TOKEN = process.env['TWITCH_OAUTH_TOKEN'];
-export const BEXXTEBOT_TOKEN = process.env['BEXXTEBOT_TOKEN'];
-export const BEXXTEBOT_SECRET = process.env['BEXXTEBOT_SECRET'];
-export const CLIENT_ID = process.env['CLIENT_ID'];
-
-// DISCORD API TOKENS: ALSO DO NOT CHANGE!!!
-export const DISCORD_TOKEN = process.env['DISCORD_TOKEN'];
-export const DISCORD_CLIENT_ID = process.env['DISCORD_CLIENT_ID'];
-
-
-/* EMOTES
-bexxteCozy 
-bexxteXcite 
-bexxteBop 
-bexxteGun 
-bexxteLove
-*/
-
+export const BEXXTEBOT_TOKEN = undefined;
+export const CLIENT_ID = undefined;
